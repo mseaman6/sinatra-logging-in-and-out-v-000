5860cd0b44806c707c6acb43c8a9558115823fb0
Monica Seaman <monica.l.seaman@gmail.com>
2018-08-07 15:31:22 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-
+    erb :account
   end
 
   get '/logout' do
@@ -30,4 +30,3 @@ class ApplicationController < Sinatra::Base
 
 
 end
-
